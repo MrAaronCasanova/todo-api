b23f596195a7b32ba0573660fa165a4a528e79cc
MrAaronCasanova <1boris231@gmail.com>
2018-02-21 10:19:24 -0800

    about to refactor todo.js routes

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
 
 let todoRoutes = require('./routes/todos');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   res.send('Basic express app is up and running');
