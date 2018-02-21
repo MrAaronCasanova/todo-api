e02b52b4520c9319878bc8845ae6b65cc3e2e2a9
MrAaronCasanova <1boris231@gmail.com>
2018-02-21 12:08:30 -0800

    building todo list functionality with api

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ let todoRoutes = require('./routes/todos');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', function (req, res) {
-  res.send('We now have a working json api!!!');
+  res.sendFile('index.html');
 });
 
 app.use('/api/todos', todoRoutes);
