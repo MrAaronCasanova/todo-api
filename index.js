2d26ae553e5d4456fa001fe9c9f7821d560e4c25
MrAaronCasanova <1boris231@gmail.com>
2018-02-21 10:39:18 -0800

    Working JSON API

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-  res.send('Basic express app is up and running');
+  res.send('We now have a working json api!!!');
 });
 
 app.use('/api/todos', todoRoutes);
 
 let port = process.env.PORT || 3000;
 app.listen(port, process.env.IP, function () {
-  console.log('Server started on port 3000!!!');
+  console.log('Server started on port 3000');
 });
